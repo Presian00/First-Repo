1b570c4d7ac3eff6425374e001342dc7494ae2e6
Presian <presianveselinov@abv.bg>
2021-02-10 09:39:02 +0200

    Add project from 10.02

---
+++ 10.02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kolko uchenici ima: ");
+            int n = int.Parse(Console.ReadLine());
+            double obshto;
+            double suma = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("vuvedete uspeha: ");
+                double u = double.Parse(Console.ReadLine());
+                suma = suma + u;
+            }
+            obshto = suma / n;
+            Console.WriteLine("{0:f2}", obshto);
+        }
+    }
+}
