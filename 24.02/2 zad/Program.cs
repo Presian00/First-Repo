dcd8850cda74b9355a31c76fa1df08b5b95a7559
Presian <presianveselinov@abv.bg>
2021-02-24 09:56:31 +0200

    Zadachi 24.02

---
+++ 24.02/2 zad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете двуично число: ");
+            string chislo = Console.ReadLine();
+            int chislo10 = Convert.ToInt32(chislo, 2);
+            Console.WriteLine("Въведете двуично число: ");
+            string chislo2 = Console.ReadLine();
+            int chislo210 = Convert.ToInt32(chislo2, 2);
+            int sum = chislo10 + chislo210;
+            string sum2 = Convert.ToString(sum, 2);
+            Console.WriteLine(sum2);
+        }
+    }
+}
---
+++ 24.02/Preubrazuvane/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Preubrazuvane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //преубразувайте десетично в двуично
+            Console.WriteLine("Въведете десетично число: ");
+            int chislo = int.Parse(Console.ReadLine());
+            string chislo2 = Convert.ToString(chislo, 2);
+            string chislo16 = Convert.ToString(chislo, 16);
+            Console.WriteLine("Десетичното е: " + chislo);
+            Console.WriteLine("Двуичното е: " + chislo2);
+            Console.WriteLine("Шеснайсетичното е:" + chislo16);
+        }
+    }
+}
